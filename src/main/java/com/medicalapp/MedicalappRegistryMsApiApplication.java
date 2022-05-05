a239d3762abea9ebc2825187a31758d58fb1bef9
21nithya <nithiyakalyani21@gmail.com>
2022-05-05 17:29:40 +0530

    first commit

---
+++ src/main/java/com/medicalapp/MedicalappRegistryMsApiApplication.java
@@ -0,0 +1,13 @@
+package com.medicalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalappRegistryMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalappRegistryMsApiApplication.class, args);
+	}
+
+}
